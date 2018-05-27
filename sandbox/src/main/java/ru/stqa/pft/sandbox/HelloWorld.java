3bc773d59e7514b85043b2bc499ff14b94170fa9
Илья Цархарт <tsarhart@MacBook-Air-Ila.local>
2018-05-27 17:01:00 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -2,6 +2,26 @@ package ru.stqa.pft.sandbox;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Hello, world");
+        hello("World");
+        hello("Ilya");
+
+        double l = 8;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+        double a = 7;
+        double b = 9;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double len) {
+        return len * len;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
     }
 }
