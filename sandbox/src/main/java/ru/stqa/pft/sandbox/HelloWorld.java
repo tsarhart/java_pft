12412ff8d318de28788fddec1d9a5002f5a4a1fa
Илья Цархарт <tsarhart@MacBook-Air-Ila.local>
2018-05-27 17:24:49 +0300

    Добавлено несколько объектов

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -5,23 +5,15 @@ public class HelloWorld {
         hello("World");
         hello("Ilya");
 
-        double l = 8;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+        Square s = new Square(8);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-        double a = 7;
-        double b = 9;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+        Rectangle r = new Rectangle(7, 9);
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello, " + somebody + "!");
     }
 
-    public static double area(double len) {
-        return len * len;
-    }
-
-    public static double area(double a, double b) {
-        return a * b;
-    }
 }
